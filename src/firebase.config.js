a933a579a53ab28a6be54e242ecd9ed63eaa2b4d
Chase Young <chasekyoung@gmail.com>
2022-04-28 10:25:59 -0400

    firebase setup

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDwBAfkXhn3Dc4glb3316Yi03nScQujFFs',
+  authDomain: 'house-marketplace-app-a6d7b.firebaseapp.com',
+  projectId: 'house-marketplace-app-a6d7b',
+  storageBucket: 'house-marketplace-app-a6d7b.appspot.com',
+  messagingSenderId: '327276784689',
+  appId: '1:327276784689:web:234c40c89b7c2ec2abb910',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
